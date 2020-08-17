5a4fde63786121c9acb8d58704df378f144c83c1
StardustGogeta <liam.gerst@gmail.com>
2020-08-17 02:31:40 -0400

    More basics in place

--- main.jsx
+++ main.jsx
@@ -6,4 +6,55 @@ class Button extends React.Component {
     }
 }
 
-ReactDOM.render(<Button />, document.getElementById("container"));
+class Semester extends React.Component {
+    render() {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Course Code</th>
+                        <th>Course Name</th>
+                        <th>Credits</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <Course />
+                    <AddCourse />
+                </tbody>
+            </table>
+        );
+    }
+}
+
+class Course extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            code: "MATH141",
+            name: "Calculus II",
+            credits: 3
+        };
+    }
+
+    render() {
+        return (
+            <tr>
+                <td>{this.state.code}</td>
+                <td>{this.state.name}</td>
+                <td>{this.state.credits}</td>
+            </tr>
+        );
+    }
+}
+
+class AddCourse extends React.Component {
+    render() {
+        return (
+            <tr>
+                <td colspan={3}><button>Click to add a course!</button></td>
+            </tr>
+        )
+    }
+}
+
+ReactDOM.render(<Semester />, document.getElementById("container"));
