11121cee8f73e641c3debaa8a6274709b52a06b3
StardustGogeta <liam.gerst@gmail.com>
2020-08-19 22:28:17 -0400

    Allow adding courses, initial work with UMD API

--- main.jsx
+++ main.jsx
@@ -1,12 +1,45 @@
-class Button extends React.Component {
-    render() {
-        return (
-            <button onClick={() => alert('Hi')}>Click me!</button>
-        );
-    }
-}
-
 class Semester extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            courses: [
+                { // Test for demonstration purposes
+                    code: "MATH141",
+                    name: "Calculus II",
+                    credits: "4"
+                }
+            ]
+        }
+        this.addCourse = this.addCourse.bind(this);
+    }
+
+    addCourse() {
+        var code = prompt("Course code?");
+        var reg = /^[A-Z]{4}[0-9]{3}$/i;
+        if (reg.test(code)) {
+            fetch("https://api.umd.io/v1/courses/" + code)
+                .then(res => res.json())
+                .then(data => {
+                    // console.log(data);
+                    var course = data[0];
+                    var code = course.course_id;
+                    var name = course.name;
+                    var credits = course.credits;
+                    var toAdd = {code: code, name: name, credits: credits};
+
+                    var newCourses = this.state.courses.concat(toAdd);
+                    this.setState({courses: newCourses});
+                })
+                .catch(err => {
+                    // UMD API could be having issues, or course code could be invalid
+                    console.log("error " + err);
+                    alert("The course search for \"" + code + "\" failed unexpectedly.");
+                });
+        } else {
+            alert("Bad input for course name \"" + code + "\". Please try again.");
+        }
+    }
+
     render() {
         return (
             <table>
@@ -18,30 +51,29 @@ class Semester extends React.Component {
                     </tr>
                 </thead>
                 <tbody>
-                    <Course />
-                    <AddCourse />
+                    {
+                        this.state.courses.map((e, i) => <Course data={e} key={i} />)
+                    }
+                    <AddCourse onClick={this.addCourse} />
                 </tbody>
             </table>
         );
     }
 }
 
+// Represents a single course row in the UI
+// Expects prop `data` object to have code, name, and credits
 class Course extends React.Component {
     constructor() {
         super();
-        this.state = {
-            code: "MATH141",
-            name: "Calculus II",
-            credits: 3
-        };
     }
 
     render() {
         return (
             <tr>
-                <td>{this.state.code}</td>
-                <td>{this.state.name}</td>
-                <td>{this.state.credits}</td>
+                <td>{this.props.data.code}</td>
+                <td>{this.props.data.name}</td>
+                <td>{this.props.data.credits}</td>
             </tr>
         );
     }
@@ -51,7 +83,7 @@ class AddCourse extends React.Component {
     render() {
         return (
             <tr>
-                <td colspan={3}><button>Click to add a course!</button></td>
+                <td colSpan={3}><button onClick={this.props.onClick}>Click to add a course!</button></td>
             </tr>
         )
     }
